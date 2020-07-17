9f4d4cf11a98a314c8e8beb38172a7a6513a6844
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-17 02:54:41 -0500

    fixed errors with source seeds.sql, changed seeds,

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,7 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", "true");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Fried Egg Burger", "true");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole", "true");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", "False");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Refried Bean Burger", "true");
+INSERT INTO burgers (burger_name) 
+VALUES 
+("Double Cheeseburger"),
+("Fried Egg Burger"),
+("Guacamole"),
+("Vegan Burger"),
+("Refried Bean Burger")
