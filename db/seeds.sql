68e7e7e228b7e5010301d4c63e0fd03ad6bf372b
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-07-17 02:38:06 -0500

    added values into seeds.sql, created server.js

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger", "true");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fried Egg Burger", "true");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Guacamole", "true");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", "False");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Refried Bean Burger", "true");
