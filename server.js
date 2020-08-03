0f69cd62ab8e14346558fa306a5927bae212474f
Ramon Garza III <59939116+Ramong06@users.noreply.github.com>
2020-08-03 12:56:14 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const routes = require('./controllers/burgers-controller');
-const exphbs = require('express-handlebars');
+var express = require("express");
 
-const PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-const app = express();
-
-app.use(express.static('public'));
+var db = require("./models");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
+app.use(express.static("public"));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+require("./routes/html-routes.js")(app);
+require("./routes/author-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
 
-app.listen(PORT, function () {
-  console.log('Server listening on: http://localhost:' + PORT);
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
